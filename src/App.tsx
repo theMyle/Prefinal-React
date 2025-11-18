07036ba6f1f428b1f6028b95368500e546178eb4
theMyle <99082612+theMyle@users.noreply.github.com>
2025-11-18 14:11:35 +0800

    initial commit
    react project scafold + tailwind + shadcn

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <div className="flex flex-col items-center">
+      <h1>Hello</h1>
+      <Button>Click Me</Button>
+    </div>
+  )
+}
+
+export default App
